c085fda017cf08e0a06eeed7a4a0c3133bbe9eee
Charlotte Morrison <charlottemaemorrison@gmail.com>
2018-11-08 15:58:17 -0500

    First few files unit 1/2

---
+++ 2-1 TwoMeaningsOfPlus/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//create a string variable and set it to your name
+      
+        
+        //print out "My name is: " and use the plus sign to add your name
+       
+        
+        //print out a statement that tells how long your name is. -use variable.length()
+        
+        
+        //explain what your favorite subject is and why you like it. 
+        //Use the plus sign to write the statement on two lines in the code.
+   
+        
+        //use the plus sign to share the course and concatenate a number for the period you have the class.
+
+	}
+
+}
